3908170831d01ac6a5aba8d51bb71c37aac39c01
animeocean <ahseangeng777@gmail.com>
2022-11-29 11:30:59 +0700

    update menu bar and make new page of seller

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,8 @@
 const shipping = document.querySelector('.currency-show');
 const shipping_option = document.querySelector('.shipping-option');
 const button_save = document.querySelector('#save');
+const account = document.querySelector('.account')
+const user_account = document.querySelector('.user-account-main')
 
 
 
@@ -26,12 +28,14 @@ function hide(element){
 
 
 function shippingOption(){
+    shipping.style.color = 'orange'
     show(shipping_option),
     button_save.addEventListener("click",function(){
         hide(shipping_option)
+        shipping.style.removeProperty("color")
     })
 }
+
 // Main----------------------------------
 hide(shipping_option)
-
 shipping.addEventListener("click",shippingOption);
